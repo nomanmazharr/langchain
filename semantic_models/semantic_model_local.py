a172765f345969c68c2cabea626a05bf4b13d9a2
Noman <nmazhar911@gmail.com>
2025-02-22 19:29:18 +0500

    optimized model loading and inference speed

--- semantic_models/semantic_model_local.py
+++ semantic_models/semantic_model_local.py
@@ -1,9 +1,14 @@
+import torch
 from langchain_huggingface import HuggingFaceEmbeddings
 import os
 
 os.environ['HF_HOME'] = 'F:/huggingface_cache'
 
-llm = HuggingFaceEmbeddings(model_name='sentence-transformers/all-MiniLM-L6-v2')
+llm = HuggingFaceEmbeddings(
+    model_name='minishlab/M2V_multilingual_output',
+    model_kwargs = {'device': 'cpu'},
+    encode_kwargs = {'normalize_embeddings': True}
+    )
 
 result = llm.embed_query('Hey! how you doing')
 
