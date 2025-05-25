39388e140b4d218a5d831349c42063d7ef2025ae
Noman <nmazhar911@gmail.com>
2025-05-25 06:29:27 +0500

    using contextual compression retriever to get the exact matching data with our query

---
+++ retrievers/contextual_compression_retriever.py
@@ -0,0 +1,67 @@
+from langchain_community.vectorstores import FAISS
+from langchain_openai import OpenAIEmbeddings, ChatOpenAI
+from langchain.retrievers.contextual_compression import ContextualCompressionRetriever
+from langchain.retrievers.document_compressors import LLMChainExtractor
+from langchain_core.documents import Document
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+# Recreate the document objects from the previous data
+docs = [
+    Document(page_content=(
+        """The Grand Canyon is one of the most visited natural wonders in the world.
+        Photosynthesis is the process by which green plants convert sunlight into energy.
+        Millions of tourists travel to see it every year. The rocks date back millions of years."""
+    ), metadata={"source": "Doc1"}),
+
+    Document(page_content=(
+        """In medieval Europe, castles were built primarily for defense.
+        The chlorophyll in plant cells captures sunlight during photosynthesis.
+        Knights wore armor made of metal. Siege weapons were often used to breach castle walls."""
+    ), metadata={"source": "Doc2"}),
+
+    Document(page_content=(
+        """Basketball was invented by Dr. James Naismith in the late 19th century.
+        It was originally played with a soccer ball and peach baskets. NBA is now a global league."""
+    ), metadata={"source": "Doc3"}),
+
+    Document(page_content=(
+        """The history of cinema began in the late 1800s. Silent films were the earliest form.
+        Thomas Edison was among the pioneers. Photosynthesis does not occur in animal cells.
+        Modern filmmaking involves complex CGI and sound design."""
+    ), metadata={"source": "Doc4"})
+]
+
+embeddings = OpenAIEmbeddings()
+vector_stores = FAISS.from_documents(
+    documents=docs,
+    embedding=embeddings
+)
+
+# requires a base retriever and compressor
+base_retriever = vector_stores.as_retriever(search_kwargs={'k':3})
+llm = ChatOpenAI()
+compressor = LLMChainExtractor.from_llm(llm)
+
+# sets a prompt to return the text that is very relevant to the query
+compression_retriever = ContextualCompressionRetriever(
+    base_retriever=base_retriever,
+    base_compressor=compressor
+)
+
+query='What is photosynthesis'
+results = compression_retriever.invoke(query)
+# print(results)
+
+# for i, doc in enumerate(results):
+#   print(f"Results: {i+1}")
+#   print(f"{doc.page_content}")
+
+
+query='history of cinema'
+results = compression_retriever.invoke(query)
+for i, doc in enumerate(results):
+  print(f"Results: {i+1}")
+  print(f"{doc.page_content}")
