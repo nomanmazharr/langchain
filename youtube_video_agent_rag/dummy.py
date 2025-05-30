fcaab6a95bd5b53d290dfae7248b72ac9851dcd5
Noman <nmazhar911@gmail.com>
2025-05-30 07:41:07 +0500

    testing voyageai with langchain

--- youtube_video_agent_rag/dummy.py
+++ youtube_video_agent_rag/dummy.py
@@ -1,15 +1,17 @@
 from dotenv import load_dotenv
 import os
-import voyageai
+from langchain_voyageai import VoyageAIEmbeddings
+# import voyageai
 
 load_dotenv()
 key = os.getenv('VOYAGE_API_KEY')
-vo = voyageai.Client(api_key=key)
+# vo = voyageai.Client(api_key=key)
+embeddings = VoyageAIEmbeddings(api_key=key, model="voyage-3")
 # This will automatically use the environment variable VOYAGE_API_KEY.
 # Alternatively, you can use vo = voyageai.Client(api_key="<your secret key>")
 
-texts = ["Sample text 1"]
+texts = ["Sample text 1", "hey how you doing", 'tell me about you']
 
-result = vo.embed(texts, model="voyage-3", input_type="document")
-print(result.embeddings)
-# print(result.embeddings[1])
+# result = embeddings.embed_query(texts)
+result = embeddings.embed_documents(texts)
+print(result)
